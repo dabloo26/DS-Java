9810edb93f8c45a0fe071702629117f0e3890b24
abhyansh <52699364+abhyansh@users.noreply.github.com>
2021-05-30 18:17:06 +0530

    Add files via upload

---
+++ Recursion/isPalindrome.java
@@ -0,0 +1,24 @@
+import java.util.*;
+class isPalindrome {
+  public static void main(String[] args) {
+    //Reverse String using recursion
+
+    Scanner sc=new Scanner(System.in);
+    String sna=new String();
+    sna=sc.nextLine();
+    boolean na=isPalindrome(sna);
+    System.out.println(na);
+  }
+
+  public static boolean isPalindrome(String s){
+    
+    if(s.length()==0||s.length()==1)
+    return true;
+
+    if(s.charAt(0)==s.charAt(s.length()-1))
+    return isPalindrome(s.substring(1,s.length()-1));
+
+    return false;
+
+ }
+}
