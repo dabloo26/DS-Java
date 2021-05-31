1f0c0efa3dbe933a94e6055d8274dae1e8f91708
abhyansh <52699364+abhyansh@users.noreply.github.com>
2021-05-31 17:23:41 +0530

    Add files via upload
    Return the first occurence of Upper Case Character using recursion

---
+++ Recursion/FirstUpperCase.java
@@ -0,0 +1,25 @@
+import java.util.*;
+class FirstUpperCase {
+  public static void main(String[] args) {
+    //Reverse String using recursion
+
+    Scanner sc=new Scanner(System.in);
+    String sna=new String();
+    sna=sc.nextLine();
+    char na=FirstUpper(sna);
+    System.out.println(na);
+  }
+
+  public static char FirstUpper(String s){
+
+    if(s.charAt(0)>='A'&& s.charAt(0)<='Z')
+    return (s.charAt(0));
+
+    if (s.length()<=1)
+    return '0';
+
+
+    return FirstUpper(s.substring(1)); 
+
+ }
+}
