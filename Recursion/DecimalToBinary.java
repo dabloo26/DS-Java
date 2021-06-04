2741a37806132d1a9cde3856eee0bfb8d7daab80
abhyansh <52699364+abhyansh@users.noreply.github.com>
2021-06-04 23:58:24 +0530

    Update DecimalToBinary.java

--- Recursion/DecimalToBinary.java
+++ Recursion/DecimalToBinary.java
@@ -8,8 +8,8 @@ class DecimalToBinary {
     System.out.print(obj.dectobin(n));
   }
   int dectobin(int n){
-    if(n==0)
-    return 0;
+    if(n==1)
+    return 1;
 
     return dectobin(n/2)*10+(n%2);
     }
