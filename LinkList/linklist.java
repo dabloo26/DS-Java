753ccbe5c86c8a81b5af639550ea633f0ecc6d8b
dabloo26 <52699364+abhyansh@users.noreply.github.com>
2021-06-16 02:15:49 +0530

    Create linklist.java
    // linklist,java learned implementing and adding value to link list 
    //print the linklist

---
+++ LinkList/linklist.java
@@ -0,0 +1,28 @@
+public class Link{
+  Node head;
+
+  public void add(int data){
+    Node node=new Node();
+    node.data=data;
+    if(head==null){
+      head=node;
+    }
+    else{
+      Node n=head;
+      while(n.next!=null){
+        n=n.next;
+      }
+      n.next=node;
+    }
+        
+  }
+
+  public void print(){
+    Node n=head;
+      while(n.next!=null){
+        System.out.print(n.data+"->");
+        n=n.next;
+      }
+      System.out.print(n.data);
+  }
+}
